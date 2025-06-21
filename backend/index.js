93e7dc68f71f437ee4ea8e82dc1b37080974b1ec
Ranjeet <kiranjeetkr80@gmail.com>
2025-06-21 19:09:06 +0530

    userUpdate

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser"; // cookie-parser frontend se jo data aata hai usse backend mein parse karne mein help karta hai..>>>
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+dotenv.config({});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http//localhost:5173',
+    Credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+//api's
+app.use("/api/v1/user", userRoute);
+
+app.listen(PORT,() =>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
