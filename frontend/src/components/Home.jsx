431a419208e4aad0d196a628d857555665db89f9
Ranjeet <kiranjeetkr80@gmail.com>
2025-06-25 23:24:20 +0530

    HeroSection

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <HeroSection/>
+        {/* <CategoryCarousel/>
+        <latestJobs/>
+        <Footer/> */}
     </div>
   )
 }
