2b0b898aaa812408bd45217169c062350e7c770a
Ranjeet <kiranjeetkr80@gmail.com>
2025-06-27 21:44:04 +0530

    profilePage

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -1,9 +1,35 @@
 
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
 
 const AppliedJobTable = () => {
   return (
-    <div>AppliedJobTable</div>
+    <div>
+        <Table>
+            <TableCaption>A list of your applied jobs</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Job Role</TableHead>
+                    <TableHead>Company</TableHead>
+                    <TableHead className="text-right">Status</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                  [1,2].map((item, index)=>(
+                    <TableRow key={index}>
+                      <TableCell>27-06-2025</TableCell>
+                      <TableCell>Fronted Developer</TableCell>
+                      <TableCell>Google</TableCell>
+                      <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                    </TableRow>
+                  ))
+                }
+            </TableBody>
+        </Table>
+    </div>
   )
 }
 
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -57,7 +57,7 @@ const Profile = () => {
                 </div>
             </div>
             <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
-                <h1>Applied Jobs</h1>
+                <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
                 {/* AppliedJobtable */}
                 <AppliedJobTable />
             </div>
