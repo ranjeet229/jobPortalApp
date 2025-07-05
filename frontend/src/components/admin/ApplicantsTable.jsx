86f31110242481cac8ac320ca83947091c090a4b
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-06 00:31:45 +0530

    application

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -1,5 +1,5 @@
 import { MoreHorizontal } from 'lucide-react'
-import React from 'react'
+import React, { use } from 'react'
 import {
     Table,
     TableBody,
@@ -10,10 +10,12 @@ import {
     TableRow
 } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { useSelector } from 'react-redux';
 
 const shortlistingStatus = ["Accepted", "Rejected"];
 
 const ApplicantsTable = () => {
+    const { applicants } = useSelector(store => store.application);
     return (
         <div>
             <Table>
@@ -29,27 +31,36 @@ const ApplicantsTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>Full Name</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Contact</TableCell>
-                        <TableCell>Resume</TableCell>
-                        <TableCell>Date</TableCell>
-                        <TableCell className="float-right cursor-pointer">
-                            <Popover>
-                                <PopoverTrigger asChild>
-                                    <MoreHorizontal className="cursor-pointer" />
-                                </PopoverTrigger>
-                                <PopoverContent className="w-32">
-                                    {shortlistingStatus.map((status, index) => (
-                                        <div key={index} className="hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
-                                            <span>{status}</span>
-                                        </div>
-                                    ))}
-                                </PopoverContent>
-                            </Popover>
-                        </TableCell>
-                    </TableRow>
+                    {
+                        applicants && applicants?.applications?.map((item) => (
+                            <TableRow key={item._id}>
+                                <TableCell>{item?.applicant?.fullname}</TableCell>
+                                <TableCell>{item?.applicant?.email}</TableCell>
+                                <TableCell>{item?.applicant?.phoneNumber}</TableCell>
+                                <TableCell>
+                                    {
+                                        item.applicant?.profile?.resume ? <a href={item?.applicant?.profile?.resumeOriginalName} target='_blank' rel='noopener noreferrer' className='text-blue-500'>{item?.applicant?.profile?.resumeOriginalName}</a> : <span>NA</span>
+                                    }
+                                    </TableCell>
+                                <TableCell>{item?.applicant.createdAt.split("T")[0]}</TableCell>
+                                <TableCell className="float-right cursor-pointer">
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <MoreHorizontal className="cursor-pointer" />
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-32">
+                                            {shortlistingStatus.map((status, index) => (
+                                                <div key={index} className="hover:bg-gray-100 px-2 py-1 rounded cursor-pointer">
+                                                    <span>{status}</span>
+                                                </div>
+                                            ))}
+                                        </PopoverContent>
+                                    </Popover>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+
                 </TableBody>
             </Table>
         </div>
