3709e1411b8ff4623fbe28d2131232e40af698c0
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-06 23:15:48 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://jobportalapp-frontend.onrender.com',
+    origin: 'https://jobportal-backend-spf8.onrender.com',
     credentials: true
 }
 app.use(cors(corsOptions));
