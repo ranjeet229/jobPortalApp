c97b2a2736473eb405a368b89b55be65e7387e46
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-07 22:59:36 +0530

    final

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Github, Linkedin } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-4 ">
+    <footer className="bg-gray-900 text-white py-4 mt-20">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
 
         <div className="text-lg font-semibold mb-4 md:mb-0">
