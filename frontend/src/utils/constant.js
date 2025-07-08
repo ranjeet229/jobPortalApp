71eeca4f30680debdc1c8dc4e624d418d178a206
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-08 15:47:05 +0530

    final

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,4 +2,3 @@ export const USER_API_END_POINT ="https://jobportalapp-backend.onrender.com/api/
 export const JOB_API_END_POINT ="https://jobportalapp-backend.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT ="https://jobportalapp-backend.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT ="https://jobportalapp-backend.onrender.com/api/v1/company";
-
