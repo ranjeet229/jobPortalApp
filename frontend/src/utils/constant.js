03b62aa9f16877500e994d149622855746f8acad
Ranjeet <kiranjeetkr80@gmail.com>
2025-10-29 21:11:06 +0530

    final

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://jobportalapprkb.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobportalapprkb.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobportalapprkb.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobportalapprkb.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://jobportalapprkback.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobportalapprkback.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobportalapprkback.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobportalapprkback.onrender.com/api/v1/company";
